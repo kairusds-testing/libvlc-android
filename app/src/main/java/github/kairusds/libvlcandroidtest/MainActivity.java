8cc47e0cd80010cf62392ef215ea6f1bd1e8ea2d
kairusds <32636532+kairusds@users.noreply.github.com>
2024-10-17 00:04:42 +0800

    Update

--- app/src/main/java/github/kairusds/libvlcandroidtest/MainActivity.java
+++ app/src/main/java/github/kairusds/libvlcandroidtest/MainActivity.java
@@ -1,9 +1,12 @@
 package github.kairusds.libvlcandroidtest;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
+// import android.view.SurfaceView;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +20,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  {
 	private static final boolean USE_TEXTURE_VIEW = false;
-	private static final boolean ENABLE_SUBTITLES = true;
+	private static final boolean ENABLE_SUBTITLES = false;
 	private static final String ASSET_FILENAME = "bbb.webm";
 
 	private VLCVideoLayout mVideoLayout = null;
@@ -25,22 +28,26 @@ public class MainActivity extends AppCompatActivity  {
 	private LibVLC mLibVLC = null;
 	private MediaPlayer mMediaPlayer = null;
 	private long time = 0L;
+	private int audioSessionId = 0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.activity_main);
+		AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+		audioSessionId = audioManager.generateAudioSessionId();
+
 		mLibVLC = new LibVLC(this, getOptions());
 		mMediaPlayer = new MediaPlayer(mLibVLC);
-		mMediaPlayer.setAudioOutput("opensles_android");
+		// mMediaPlayer.setAudioOutput("opensles_android");
 
 		mVideoLayout = findViewById(R.id.video_layout);
 	}
 
 	private ArrayList<String> getOptions(){
 		final ArrayList<String> options = new ArrayList<>();
-		options.add("--no-audio-time-stretch");
+		// options.add("--no-audio-time-stretch");
 		options.add("--avcodec-skiploopfilter");
 		options.add("-1");
 		options.add("--avcodec-skip-frame");
@@ -51,6 +58,7 @@ public class MainActivity extends AppCompatActivity  {
 		options.add("soxr"); // use soxr for audio resampling
 		options.add("-v"); // minimum vlc logging
 		options.add("--preferred-resolution=-1"); // use maximum resolution
+		options.add("--audiotrack-session-id=" + audiotrackSessionId);
 
 		return options;
 	}
@@ -97,6 +105,19 @@ public class MainActivity extends AppCompatActivity  {
 			throw new RuntimeException("Invalid asset folder");
 		}
 		mMediaPlayer.play();
+		// for(int i = 0; i < mVideoLayout.getChildCount(); i++){
+		//	View view = mVideoLayout.getChildAt(i);
+		//	if(view instanceof SurfaceView){
+		int surfaceVideoId = getResources().getIdentifier("surface_video", "id", "org.videolan");
+		mVideoLayout.findViewById(surfaceVideoId).setOnClickListener(v -> {
+			if(mMediaPlayer.isPlaying()){
+				mMediaPlayer.pause();
+			}else{
+				mMediaPlayer.resume();
+			}
+		});
+		//	}
+		// }
 	}
 
 	@Override
